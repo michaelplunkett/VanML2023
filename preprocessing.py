79c89927b6c4f48bce3fa6dcab4862ebd0343427
Michael Plunkett <mic.a.plunkett@gmail.com>
2023-09-24 12:35:24 -0700

    Converted TimeofDay to seconds

--- preprocessing.py
+++ preprocessing.py
@@ -10,15 +10,16 @@ calls = calls[["callreportnum","DateStart","TimeStart","CityName","ContactMethod
 
 # Convert Timestamp to Datetime
 calls['DateStart'] = pd.to_datetime(calls['DateStart']).dt.date
-calls['TimeofDay'] = pd.to_datetime(calls['TimeStart']).dt.time
+calls['TimeStart'] = pd.to_datetime(calls['TimeStart']).dt.time
 
 # Create time columns
-calls['DateTimeStart'] = calls.apply(lambda x: dt.datetime.combine(x.DateStart, x.TimeofDay), axis=1)
+calls['DateTimeStart'] = calls.apply(lambda x: dt.datetime.combine(x.DateStart, x.TimeStart), axis=1)
 calls['AbsoluteTime'] = calls['DateTimeStart'].apply(lambda x: x.timestamp())
 calls['TimeofYear'] = calls['DateTimeStart'].apply(lambda x: x.timestamp() - dt.datetime(x.year, 1, 1).timestamp())
+calls['TimeofDay'] = calls['DateTimeStart'].apply(lambda x: x.timestamp() - dt.datetime(x.year, x.month, x.day).timestamp())
+
 
 calls = calls.drop(['DateStart', 'TimeStart'], axis=1)
-
 
 # Resolve NA city names
 calls.loc[calls["CityName"].isna(),"CityName"] = "Unknown"
@@ -26,7 +27,6 @@ calls.loc[calls["CityName"].isna(),"CityName"] = "Unknown"
 # calls['month'] = calls['DateTimeStart'].apply(lambda x: x.month)
 # calls['day'] = calls['DateTimeStart'].apply(lambda x: x.day)
 # print(calls[calls['month'] + calls['day'] == 2][['DateTimeStart', 'TimeofYear']])
-
 
 print(calls.dtypes)
 print(calls)
